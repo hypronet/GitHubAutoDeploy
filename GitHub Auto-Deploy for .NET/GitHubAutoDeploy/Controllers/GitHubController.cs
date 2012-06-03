ed5c463330edc7389b0bfa607d76561c5e53bf04
Juliën Hanssens <github@hanssens.com>
2012-06-03 15:18:56 +0200

    Added GitHubController
    - Skeleton setup for GitHub's Post-Receive service

---
+++ GitHub Auto-Deploy for .NET/GitHubAutoDeploy/Controllers/GitHubController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubAutoDeploy.Controllers
+{
+    public class GitHubController : Controller
+    {
+
+        /// <summary>
+        /// Listener for GitHub Post-Receive services.
+        /// </summary>
+        /// <param name="args">Arguments that are provided as a Json string by GitHub.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Update(string args)
+        {
+            var value = Json(args);
+            ViewBag.Args = value;
+            return View();
+        }
+
+    }
+}
